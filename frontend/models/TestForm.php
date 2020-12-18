369d50fbf7d5358c1ab9eadea0f799574681837d
QUNETOL57 <an.1998@yandex.ru>
2020-12-18 11:00:28 +0300

    lesson 11

--- frontend/models/TestForm.php
+++ frontend/models/TestForm.php
@@ -12,4 +12,32 @@ class TestForm extends Model
     public $email;
     public $text;
 
+    public function attributeLabels()
+    {
+        return [
+            'name' => 'Имя',
+            'email' => 'E-mail',
+            'text' => 'Текст сообщения',
+
+        ];
+    }
+
+    public function rules()
+    {
+        return [
+            [['name', 'email'], 'required'],
+            ['email','email'],
+//            ['name', 'string', 'min' => 2, 'max' => 5],
+            ['name', 'string', 'length' => [2,6]],
+            ['name', 'myRule'],
+            ['text', 'trim'],
+        ];
+    }
+
+    public function myRule($attrs){
+        if( !in_array($this->$attrs, ['hello', 'world'])){
+            $this->addError($attrs, 'Wrong!');
+        }
+    }
+
 }
--- frontend/views/layouts/basic.php
+++ frontend/views/layouts/basic.php
@@ -6,42 +6,42 @@ AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?= $this->title ?></title>
-    <?php $this->registerCsrfMetaTags() ?>
+<html lang="<?= Yii::$app->language ?>">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?= $this->title ?></title>
+        <?php $this->registerCsrfMetaTags() ?>
 
-    <?php $this->head() ?>
-</head>
-<body>
-<?php $this->beginBody() ?>
-    <div class="wrap">
-        <div class="container">
+        <?php $this->head() ?>
+    </head>
+    <body>
+    <?php $this->beginBody() ?>
+        <div class="wrap">
+            <div class="container">
 
-            <ul class="nav nav-pills">
-                <li class="nav-item active">
-                   <?= Html::a('Главная','/web/')?>
-                </li>
-                <li class="nav-item">
-                    <?= Html::a('Статьи',['post/index'])?>
-                </li>
-                <li class="nav-item">
-                    <?= Html::a('Статья',['post/show'])?>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
-            </ul>
-            <?php if( isset($this->blocks['block1'])): ?>
-                <?php echo $this->blocks['block1'] ?>
-            <?php endif;?>
+                <ul class="nav nav-pills">
+                    <li class="nav-item active">
+                       <?= Html::a('Главная','/web/')?>
+                    </li>
+                    <li class="nav-item">
+                        <?= Html::a('Статьи',['post/index'])?>
+                    </li>
+                    <li class="nav-item">
+                        <?= Html::a('Статья',['post/show'])?>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+                <?php if( isset($this->blocks['block1'])): ?>
+                    <?php echo $this->blocks['block1'] ?>
+                <?php endif;?>
 
-            <?= $content ?>
+                <?= $content ?>
+            </div>
         </div>
-    </div>
-<?php $this->endBody() ?>
-</body>
+    <?php $this->endBody() ?>
+    </body>
 </html>
 <?php $this->endPage() ?>
